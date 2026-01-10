f1b751d65cc7b528b18fb5b9d701d32ff642b9c7
Kirill Gusev <myaumuraa@gmail.com>
2026-01-10 22:38:39 +0300

    feat: Add folder button and mock view

--- til/ContentView.swift
+++ til/ContentView.swift
@@ -10,13 +10,14 @@ import Dependencies
 import GRDB
 import StructuredQueriesCore
 import SQLiteData
+import PathKit
 
 struct ContentView: View {
-    
+
     @Environment(\.openWindow) private var openWindow
-    
+
     @State private var selectedNote: Note?
-    
+
     var body: some View {
         NavigationSplitView {
             List {
@@ -26,14 +27,31 @@ struct ContentView: View {
         } content: {
             NotesList(viewModel: NotesListViewModel(), selectedNote: $selectedNote)
         } detail: {
-            if let selectedNote {
-                NoteDetailView(viewModel: NoteDetailViewModel(note: selectedNote))
-            }
+            DetailView()
         }
+        .navigationSplitViewStyle(.balanced)
         .toolbar {
             Button("", systemImage: "plus") {
                 openWindow(id: "note")
             }
+        }
+        .toolbar {
+            Button("", systemImage: "folder") {
+                let path = PathKit.Path.home + "Markdowns"
+                NSWorkspace.shared.open(path.url)
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func DetailView() -> some View {
+        if let selectedNote {
+            let viewModel = NoteDetailViewModel(note: selectedNote)
+            NoteDetailView(viewModel: viewModel)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        } else {
+            ContentUnavailableView("Select the note!", systemImage: "note")
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
         }
     }
 }
